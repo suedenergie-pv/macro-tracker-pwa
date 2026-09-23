39af8a00b19ceaa92983af00886dc70933897b6d
suedenergie-pv <info@suedenergie-pv.de>
2026-09-23 13:51:12 +0200

    Fix GitHub Pages base path

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,17 @@
-const CACHE_NAME = 'macro-tracker-7c64854a59a2cbb2';
-const INDEX_URL = '/macro-tracker/';
+const CACHE_NAME = 'macro-tracker-6a82747b69e416c2';
+const INDEX_URL = '/macro-tracker-pwa/';
 const PRECACHE_URLS = [
-  "/macro-tracker/",
-  "/macro-tracker/.nojekyll",
-  "/macro-tracker/404.html",
-  "/macro-tracker/icon-180.png",
-  "/macro-tracker/icon-192.png",
-  "/macro-tracker/icon-512.png",
-  "/macro-tracker/index.html",
-  "/macro-tracker/manifest.webmanifest",
-  "/macro-tracker/metadata.json",
-  "/macro-tracker/_expo/static/js/web/index-03a066d15a86c2d6914bf891273d27bf.js",
-  "/macro-tracker/_expo/static/js/web/index-46f6615a318802945c0f6fb0f1d079c2.js"
+  "/macro-tracker-pwa/",
+  "/macro-tracker-pwa/.nojekyll",
+  "/macro-tracker-pwa/404.html",
+  "/macro-tracker-pwa/icon-180.png",
+  "/macro-tracker-pwa/icon-192.png",
+  "/macro-tracker-pwa/icon-512.png",
+  "/macro-tracker-pwa/index.html",
+  "/macro-tracker-pwa/manifest.webmanifest",
+  "/macro-tracker-pwa/metadata.json",
+  "/macro-tracker-pwa/_expo/static/js/web/index-03a066d15a86c2d6914bf891273d27bf.js",
+  "/macro-tracker-pwa/_expo/static/js/web/index-46f6615a318802945c0f6fb0f1d079c2.js"
 ];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(PRECACHE_URLS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key.startsWith('macro-tracker-') && key !== CACHE_NAME).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
